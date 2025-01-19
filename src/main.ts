ece2c00b7ca14cfc930ef40c46296618fdcd63c8
Victor Toschi <victor.toschi@hotmail.com>
2025-01-19 14:11:19 -0300

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
