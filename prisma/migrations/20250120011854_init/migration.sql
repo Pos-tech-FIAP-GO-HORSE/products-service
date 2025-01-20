9398850de51038e346115f362887ad40a4ca7c44
Victor Toschi <victor.toschi@hotmail.com>
2025-01-19 22:20:09 -0300

    Update table format

--- prisma/migrations/20250120011854_init/migration.sql
+++ prisma/migrations/20250120011854_init/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE `Product` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `publicId` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `category` ENUM('Lanche', 'Acompanhamento', 'Bebida', 'Sobremesa') NOT NULL,
     `price` DOUBLE NOT NULL,
@@ -11,5 +12,6 @@ CREATE TABLE `Product` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `Product_publicId_key`(`publicId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
