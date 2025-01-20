341b42fb4f60e8129d3c853828f6825cfcf9065b
Victor Toschi <victor.toschi@hotmail.com>
2025-01-20 20:23:28 -0300

    Create CreateProductUseCase

---
+++ src/usecases/createProductUseCase.ts
@@ -0,0 +1,18 @@
+import type { Product } from "../domain/entities/product";
+import type { ProductRepository } from "../domain/repositories/productRepository";
+import { v4 as uuidv4 } from "uuid";
+
+export class CreateProductUseCase {
+  constructor(private productRepository: ProductRepository) {}
+
+  async execute(product: Omit<Product, "id" | "publicId" | "createdAt" | "updatedAt">): Promise<Product> {
+    const newProduct: Omit<Product, "id"> = {
+      ...product,
+      publicId: uuidv4(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    return this.productRepository.create(newProduct);
+  }
+}
