7b50d7b8508112a3162b44376466f8b19d366b01
Victor Toschi <victor.toschi@hotmail.com>
2025-01-29 20:41:00 -0300

    feat: add new messages

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -4,4 +4,5 @@ export const MESSAGES = {
   ERROR_FETCHING_PRODUCTS: 'Error fetching products',
   ERROR_FETCHING_PRODUCT: 'Error fetching product',
   PRODUCT_NOT_FOUND: 'Product not found',
+  ERROR_DELETING_PRODUCT: 'Error deleting product',
 } as const;
