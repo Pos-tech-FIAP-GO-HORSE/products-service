516601d97160a3731951be1fabc0a9012ab059df
Victor Toschi <victor.toschi@hotmail.com>
2025-01-28 21:52:03 -0300

    adjust controller

--- src/adapters/controllers/productController.ts
+++ src/adapters/controllers/productController.ts
@@ -1,19 +1,29 @@
-import type { Request, Response } from "express";
-import { CreateProductUseCase } from "../../usecases/createProductUseCase";
-import { PrismaProductRepository } from "../../infrastructure/repositories/prismaProductRepository";
+import type { Request, Response } from 'express';
+import { CreateProductUseCase } from '../../usecases/createProductUseCase';
+import { PrismaProductRepository } from '../../infrastructure/repositories/prismaProductRepository';
+import { HTTP_STATUS } from '../../constants/httpStatusCodes';
+import { MESSAGES } from '../../constants/messages';
 
 const productRepository = new PrismaProductRepository();
 const createProductUseCase = new CreateProductUseCase(productRepository);
 
-export const ProductController = {
-  async createProduct(req: Request, res: Response) {
-    try {
-      const productData = req.body;
-      const product = await createProductUseCase.execute(productData);
+async function createProduct(req: Request, res: Response) {
+	try {
+		const productData = req.body;
+		const product = await createProductUseCase.execute(productData);
 
-      res.status(201).json(product);
-    } catch (error) {
-      res.status(400).json({ error: (error as Error).message });
-    }
-  },
+		res.status(HTTP_STATUS.CREATED).json({
+			message: MESSAGES.PRODUCT_CREATED,
+			product,
+		});
+	} catch (error) {
+		res.status(HTTP_STATUS.BAD_REQUEST).json({
+			message: MESSAGES.ERROR_CREATING_PRODUCT,
+			error: (error as Error).message,
+		});
+	}
+}
+
+export const ProductController = {
+	createProduct,
 };
