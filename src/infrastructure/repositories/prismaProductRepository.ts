802b11137133cf4aaad666dffe2e245ead9be84d
Victor Toschi <victor.toschi@hotmail.com>
2025-01-28 21:53:17 -0300

    adjust dependency injection

--- src/infrastructure/repositories/prismaProductRepository.ts
+++ src/infrastructure/repositories/prismaProductRepository.ts
@@ -1,18 +1,18 @@
-import { PrismaClient } from "@prisma/client";
-import type { Product } from "../../domain/entities/product";
-import type { ProductRepository } from "../../domain/repositories/productRepository";
-
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client';
+import type { Product } from '../../domain/entities/product';
+import type { ProductRepository } from '../../domain/repositories/productRepository';
 
 export class PrismaProductRepository implements ProductRepository {
-  async create(product: Omit<Product, "id">): Promise<Product> {
-    const prismaProduct = await prisma.product.create({
-      data: product,
-    });
+	constructor(private prisma: PrismaClient = new PrismaClient()) {}
 
-    return {
-      ...prismaProduct,
-      id: prismaProduct.id.toString(),
-    };
-  }
+	async create(product: Omit<Product, 'id'>): Promise<Product> {
+		const prismaProduct = await this.prisma.product.create({
+			data: product,
+		});
+
+		return {
+			...prismaProduct,
+			id: prismaProduct.id.toString(),
+		};
+	}
 }
