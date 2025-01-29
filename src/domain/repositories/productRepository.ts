3ade0b51d5a1ac57dfde0adb890cc5a2d631c42f
Victor Toschi <victor.toschi@hotmail.com>
2025-01-29 19:55:39 -0300

    feat: add get methods

--- src/domain/repositories/productRepository.ts
+++ src/domain/repositories/productRepository.ts
@@ -2,4 +2,7 @@ import type { Product } from '../entities/product';
 
 export interface ProductRepository {
   create(product: Omit<Product, 'id' | 'createdAt'>): Promise<Product>;
+  getAll(): Promise<Product[]>;
+  getById(id: string): Promise<Product | null>;
+  getByCategory(category: Product['category']): Promise<Product[]>;
 }
