408f3099d3d071b82d0cc383ddfb9cffd8d3fa4f
Victor Toschi <victor.toschi@hotmail.com>
2025-01-28 21:51:40 -0300

    run lint

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import dotenv from "dotenv";
-import swaggerJsdoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-import swaggerOptions from "./config/swagger";
-import productRoutes from "./adapters/routes/products";
+import express from 'express';
+import dotenv from 'dotenv';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import swaggerOptions from './config/swagger';
+import productRoutes from './adapters/routes/products';
 
 dotenv.config();
 
@@ -14,8 +14,8 @@ const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use("/products", productRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/products', productRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
